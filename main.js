c8e44d153e9744980241216f60e0e0abf7a32932
Bruno2204 <brunoegama@gmail.com>
2024-05-21 12:47:54 -0300

    Update main.js

--- main.js
+++ main.js
@@ -5,7 +5,10 @@ msjLindos = ['Miamor','Preciosa','Te quiero mucho','Mi bb <3','Me gusta mucho tu
     'Mi esquizauria linda','Mi esquiza','Lindona','Preciosona','Mi esquizotomica nuclear pechocha','Pechocha','Me gustan mucho tus besitos','Tu sonrisa es uno de mis mayores tesoros (junto con tus 😏😏)',
     'Cocinas muy rico','Tu 🍒 es una de las cosas mas lindas en el universo universal','Sos casi tan esquiza como linda','Cada dia sos mas hermosa que el anterior','Estoy muy orgulloso de vos y de todo lo que haces',
     'Mi esquizopreciosa','Sos la novia mas linda del mundo mundial','Es muy lindo tenerte cada dia conmigo','Cada vez q sonreis, nace un hada','Todavia no puedo creer todo el tiempo que llevamos',
-    'Me encanta tenerte a mi lado','Mi mujer','Mi señora','Mi novia precilindhermosa'
+    'Me encanta tenerte a mi lado','Mi mujer','Mi señora','Mi novia precilindhermosa','Conocerte es una de las mejores cosas q me paso en la vida','Mas te vale q siempre seas mi esquizona linda',
+    'Aunque seas una enojona te ves linda cuando te enojas','<3','<3<3<3','♥♥♥','BALAAAAAAAAAAA','Tenemos q terminar death note','Mi abogadona linda','Tenes una hermosa sonrisa',
+    'Mi pechocha','Tus besitos son muy lindos','Sos la mejor novia del mundo','MIAAAAaAaAAaaaaAa5654da6f5sa6ad','balita<3',':3','Quiero criar un gato con vos',
+    'Q lindo es haberte conocido','Mi pechochochochochochcocha','Mi enojona','Mi enojosauria','Tus tetas son DEMASIADO lindas <3'
 ]
 msjActual = 'Miamor'
 
